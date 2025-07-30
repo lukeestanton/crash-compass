c2daaf3d4eaa55a73e767ca2614d0ae031a0d200
lukeestanton <lukeestanton@gmail.com>
2025-07-30 14:08:22 -0400

    ESLinting and configuration updates

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e1564af2fa48860d2dd2c214aeb9a0d8f86701")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdaf6767b1acd647289b7766945b14b221bbf0dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
